0b92253a930a4f05f571b7ebf632febc96c18a57
Sanfra1407 <1427939+Sanfra1407@users.noreply.github.com>
2021-03-25 00:20:55 +0100

    Update Flow.vue
    Add formactCurrency

--- src/components/Flow.vue
+++ src/components/Flow.vue
@@ -2,11 +2,11 @@
   <transition name="fade">
     <tr v-if="flow">
       <td class="has-text-centered">
-        <span>{{ amount }} {{ currency }}</span>
+        <span>{{ currency }} {{ formattedAmount }}</span>
       </td>
       <td class="has-text-centered">
         <i class="fa fa-arrow-right has-text-danger" v-if="flowTypeIsACost" />
-        <i class="fa fa-arrow-left has-text-success" v-else/>
+        <i class="fa fa-arrow-left has-text-success" v-else />
       </td>
       <td class="has-text-centered">
         <button class="button is-danger is-small" @click="removeRow(index)">
@@ -18,9 +18,16 @@
 </template>
 
 <script>
+import { formatCurrency } from '../utils';
+
 export default {
   props: {
     currency: {
+      type: String,
+      required: true,
+    },
+
+    currencyFormat: {
       type: String,
       required: true,
     },
@@ -42,14 +49,14 @@ export default {
   },
 
   computed:{
-    amount() {
-      const price = this.flow.amount.toFixed(2);
-
-      return price.replace('.', ',');
-    },
-
     flowTypeIsACost() {
       return this.flow.type === 'cost';
+    },
+
+    formattedAmount() {
+      const {amount} = this.flow;
+
+      return formatCurrency(this.currencyFormat, amount);
     }
   }
 }
