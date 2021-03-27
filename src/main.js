067af2a9bd02ff92cf7edd6fbe5d6b8810e41cb0
Sanfra1407 <1427939+Sanfra1407@users.noreply.github.com>
2021-03-28 00:37:19 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueCashFlow from './VueCashFlow';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el: '#vcf',
+  render: h => h(VueCashFlow)
+});
