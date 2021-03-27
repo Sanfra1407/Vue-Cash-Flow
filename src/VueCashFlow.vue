643c3342656b3b0f0dece4bc83c67a3d3f392568
Sanfra1407 <1427939+Sanfra1407@users.noreply.github.com>
2021-03-28 00:37:14 +0100

    Create VueCashFlow.vue
    Renamed App.vue in VueCashFlow

---
+++ src/VueCashFlow.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1 class="title has-text-centered">Vue Cash Flow</h1>
+    <CashFlow />
+  </div>
+</template>
+
+<script>
+import CashFlow from './components/CashFlow';
+
+export default {
+  name: 'VueCashFlow',
+  components: {
+    CashFlow
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'bulma';
+  @import '~bulma-tooltip';
+
+  #app {
+    margin-top: 50px;
+  }
+
+  .fa + *{
+    margin-left: 5px;
+  }
+
+  .select {
+    &,
+    select {
+      width: 100%;
+    }
+  }
+</style>
